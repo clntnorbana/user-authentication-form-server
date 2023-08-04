b35700c52a955d9b92e54ce65c67ae5598ef0380
clinton otbana <rbnclntn@gmail.com>
2023-08-04 18:14:39 +0800

    commit

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
@@ -23,28 +21,25 @@ app.set("trust proxy", 1);
 //   next();
 // });
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "https://user-authentication-server.onrender.com",
-    // here
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = [
+        "http://localhost:5173",
+        "https://user-authentication-server.onrender.com",
+        // here
+    ];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
 });
 // routes
 app.use("/api/authentication", authRoute_1.default);
 app.use("/api/user", userRoute_1.default);
 (0, database_1.default)();
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+    console.log(`listening on port ${port}`);
 });
