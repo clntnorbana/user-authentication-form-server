f7e5109d00dd73e46b3b1ed1061ec9b2d6e2fbde
clinton otbana <rbnclntn@gmail.com>
2023-08-04 17:17:45 +0800

    PATCH change to PUT

--- dist/routes/userRoute.js
+++ dist/routes/userRoute.js
@@ -4,7 +4,19 @@ const express_1 = require("express");
 const userController_1 = require("../controllers/userController");
 const authentication_1 = require("../middlewares/authentication");
 const router = (0, express_1.Router)();
-router.get("/profile", authentication_1.requireAuth, userController_1.getUserProfile);
-router.patch("/profile", authentication_1.requireAuth, userController_1.updateUserProfile);
-router.delete("/profile", authentication_1.requireAuth, userController_1.deleteUserAccount);
+router.get(
+  "/profile",
+  authentication_1.requireAuth,
+  userController_1.getUserProfile
+);
+router.put(
+  "/profile",
+  authentication_1.requireAuth,
+  userController_1.updateUserProfile
+);
+router.delete(
+  "/profile",
+  authentication_1.requireAuth,
+  userController_1.deleteUserAccount
+);
 exports.default = router;
