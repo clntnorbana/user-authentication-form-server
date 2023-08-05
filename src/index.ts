ec1f4c821dfa3d65b0c2823c81041816b1c6719f
clinton otbana <rbnclntn@gmail.com>
2023-08-05 14:22:06 +0800

    cors edited

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ app.set("trust proxy", true);
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", req.header("Origin") || "*");
   res.removeHeader("x-powered-by");
-  res.setHeader("Access-Control-Allow-Origin", req.method);
+  res.setHeader("Access-Control-Allow-Methods", req.method);
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
